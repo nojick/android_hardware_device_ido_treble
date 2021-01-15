2f85bcd2e9acaa93e0cf31a8802aa879b6e2ec9c
nojick <nojick@gmail.com>
2021-01-15 22:04:36 +0300

    msm8916: ido: bluetooth and widevine
    The Bluetooth service from oreo msm8916, as well as the drm widevine service, requires libraries from vndk below the version, so we will add them and make them shim libraries.

--- device.mk
+++ device.mk
@@ -346,6 +346,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so \
+
+
 # Wifi
 PRODUCT_PACKAGES += \
     libwcnss_qmi \
