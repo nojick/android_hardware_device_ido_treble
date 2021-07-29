2abe17a9997b33d56e51413bf367fc3a61e85023
nojick <nojick@gmail.com>
2021-07-29 11:05:15 +0300

    ido: camera service

--- device.mk
+++ device.mk
@@ -135,6 +135,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     camera.msm8916 \
     libmm-qcamera \
     libshim_camera \
