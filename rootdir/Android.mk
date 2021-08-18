d0b8ac83b8ba07bcaa42bec344167135087aebb2
nojick <nojick@gmail.com>
2021-08-18 11:30:52 +0300

    ido: fix the file name

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -52,6 +52,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.rc
+LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
