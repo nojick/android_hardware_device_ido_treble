5331d4ef813e757431d480db2edb64fda3ea3b19
nojick <nojick@gmail.com>
2021-09-10 12:07:07 +0300

    ido: Sepolicy:  Comment markers, this requires attention and improvement.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,11 +193,12 @@ TARGET_PER_MGR_ENABLED := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#include device/qcom/sepolicy/legacy-sepolicy.mk
+#include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/xiaomi/ido/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/ido/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/xiaomi/ido/sepolicy/public
-BOARD_SEPOLICY_VERS := 27.0
+#BOARD_SEPOLICY_VERS := 27.0
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
