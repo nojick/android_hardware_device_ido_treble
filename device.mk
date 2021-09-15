09af48e8364b4530f67b22306ed0c3f46f004e31
nojick <nojick@gmail.com>
2021-09-15 11:30:13 +0300

    ido: update service widevine drm

--- device.mk
+++ device.mk
@@ -110,8 +110,10 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.3-service.clearkey \
+    libprotobuf-cpp-lite
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
