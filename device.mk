14cfcdd3dfcb0b521e5d7f1a9d86ef6df52a233c
nojick <nojick@github.com>
2021-10-23 18:10:16 +0300

    general comment

--- device.mk
+++ device.mk
@@ -84,14 +84,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0 \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
+    android.hardware.audio@5.0 \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.common@5.0 \
+    android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.effect@5.0 \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -106,7 +108,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.msm8916 \
     android.hardware.sensors@1.0-service.msm8916 \
-    sensors.msm8916
+    sensors.msm8916 \
+    libsensorndkbridge
 
 # Thermal
 PRODUCT_PACKAGES += \
