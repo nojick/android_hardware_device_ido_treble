dd239919196f660e21d172a36ff2fb311f2494da
nojick <nojick@github.com>
2021-11-18 22:05:45 +0300

    ido: update service audio  to 6.0

--- device.mk
+++ device.mk
@@ -84,14 +84,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0 \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
+    android.hardware.audio@6.0 \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.common@6.0 \
+    android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    android.hardware.soundtrigger@2.2-impl \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
