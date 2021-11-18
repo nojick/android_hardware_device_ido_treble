df23c4113ff210a655ec74636ef99779db48b749
nojick <nojick@github.com>
2021-11-18 21:58:03 +0300

    ido: update service health to 2.1
    You'll come back if it's necessary

--- device.mk
+++ device.mk
@@ -314,8 +314,8 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # USB
 PRODUCT_PACKAGES += \
