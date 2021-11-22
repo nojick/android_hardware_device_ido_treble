e51f1cf3e9bbb1bac66725bd62c2f4fe0e700084
nojick <nojick@github.com>
2021-11-22 17:30:07 +0300

    ido: thermal qti new a11

--- device.mk
+++ device.mk
@@ -112,9 +112,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8916
+    android.hardware.thermal@2.0-service.qti
 
 # DRM
 PRODUCT_PACKAGES += \
