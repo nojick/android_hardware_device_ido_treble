d03169f445fd1c89b5d869a6e9d84867e50014bd
nojick <nojick@github.com>
2021-11-24 18:59:55 +0300

    ido: camera: a11
    vold.decrypt's value isn't just a simple 1 or 0 integer string (see
    https://source.android.com/security/encryption/full-disk). atoi will
    always return 0 for invalid conversions, so removing this check affects
    nothing, really.
    https://github.com/PixelExperience-Devices/device_xiaomi_land/commit/e5c483f78afa267dcecc7b336b4a63d11587687b#diff-121b89b509671ec6c53bfd4c3e7a6c670a80b52bdf75fc12937d7d87a6122f07

--- device.mk
+++ device.mk
@@ -150,9 +150,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-service.ido \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.5-service \
     camera.msm8916 \
     libmm-qcamera \
     Snap
