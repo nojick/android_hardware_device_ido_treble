f01cf979cbe02520d04032a5e72be69c348aff58
nojick <nojick@github.com>
2021-11-24 16:24:08 +0300

    ido: service fingerprint

--- device.mk
+++ device.mk
@@ -195,7 +195,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprint.msm8916 \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service.ido
 
 # FM
 PRODUCT_PACKAGES += \
