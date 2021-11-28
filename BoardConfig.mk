73fa7a5bd39fe956ff8b34522f6986253bca4412
nojick <nojick@github.com>
2021-11-28 15:44:14 +0300

    ido: sepolicy permissive to enforce

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,7 +65,7 @@ BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom ehci-hcd.park=3 a
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x02000000
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8916
@@ -203,7 +203,7 @@ include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/xiaomi/ido/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/ido/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/xiaomi/ido/sepolicy/public
-BOARD_SEPOLICY_VERS := 28.0
+BOARD_SEPOLICY_VERS := 30.0
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wifi
