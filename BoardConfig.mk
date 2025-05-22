77cb4af1babfc8bd74df58c0c4456461b5e0ede0
nojick <nojick@gmail.com>
2025-05-23 00:26:53 +0300

    ido: Set cmdline path firmware
    it will be easier to debug this way.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ BUILD_BROKEN_DUP_RULES := true
 ENABLE_CPUSETS := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE := firmware_class.path=/vendor/firmware console=none androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x02000000
