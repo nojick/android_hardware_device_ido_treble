a8a77ba8c0a8c462440b4ac640d1368c41ed17ff
nojick <nojick@gmail.com>
2025-05-26 23:26:42 +0300

    Revert "sdm: Add DRC support for WFD"
    This reverts commit c4668719c37d9bd786c027c520de85a23acf288c.

--- display-hal/sdm/libs/core/drm/hw_device_drm.cpp
+++ display-hal/sdm/libs/core/drm/hw_device_drm.cpp
@@ -732,7 +732,7 @@ DisplayError HWDeviceDRM::Validate(HWLayers *hw_layers) {
 
   int ret = drm_atomic_intf_->Validate();
   if (ret) {
-    DLOGE("failed with error %d for %s", ret, device_name_);
+    DLOGE("%s failed with error %d", __FUNCTION__, ret);
     return kErrorHardware;
   }
 
@@ -877,6 +877,8 @@ void HWDeviceDRM::SetRect(const LayerRect &source, DRMRect *target) {
 bool HWDeviceDRM::EnableHotPlugDetection(int enable) {
   return true;
 }
+
+void HWDeviceDRM::ResetDisplayParams() {}
 
 DisplayError HWDeviceDRM::SetCursorPosition(HWLayers *hw_layers, int x, int y) {
   DTRACE_SCOPED();
@@ -1108,7 +1110,6 @@ void HWDeviceDRM::UpdateMixerAttributes() {
   mixer_attributes_.split_left = display_attributes_.is_device_split
                                      ? hw_panel_info_.split_info.left_split
                                      : mixer_attributes_.width;
-  DLOGI("Mixer WxH %dx%d for %s", mixer_attributes_.width, mixer_attributes_.height, device_name_);
 }
 
 }  // namespace sdm
--- display-hal/sdm/libs/hwc2/hwc_display.cpp
+++ display-hal/sdm/libs/hwc2/hwc_display.cpp
@@ -1367,9 +1367,10 @@ void HWCDisplay::DumpInputBuffers() {
     auto acquire_fence_fd = layer->input_buffer.acquire_fence_fd;
 
     if (acquire_fence_fd >= 0) {
-      DisplayError error = buffer_allocator_->MapBuffer(pvt_handle, acquire_fence_fd);
-      if (error != kErrorNone) {
-        continue;
+      int error = sync_wait(acquire_fence_fd, 1000);
+      if (error < 0) {
+        DLOGW("sync_wait error errno = %d, desc = %s", errno, strerror(errno));
+        return;
       }
     }
 
