LineageOS/android_hardware_qcom_display

hwc2: Remove power manager dependency
Remove power manager dependency as libpowermanager is
not accessible for hal.

Change-Id: Ibcddc7fc8f04b530fc7d96080d4d6c2025e76db7

sdm/libs/hwc2/Android.mk
@@ -24,7 +24,7 @@ LOCAL_CLANG                   := true
 
 LOCAL_SHARED_LIBRARIES        := libsdmcore libqservice libbinder libhardware libhardware_legacy \
                                  libutils libcutils libsync libqdutils libqdMetaData libdl \
-                                 libpowermanager libsdmutils libc++ liblog libgrallocutils
+                                 libsdmutils libc++ liblog libgrallocutils
 
 ifneq ($(TARGET_USES_GRALLOC1), true)
     LOCAL_SHARED_LIBRARIES += libmemalloc

nojick/android_hardware_device_ido_treble

display-hal/sdm/libs/hwc2/Android.mk
@@ -19,7 +19,7 @@ LOCAL_CLANG                   := true
 
 LOCAL_SHARED_LIBRARIES        := libsdmcore libqservice libbinder libhardware libhardware_legacy \
                                  libutils libcutils libsync libqdutils libqdMetaData libdl \
-                                 libpowermanager libsdmutils libc++ liblog libdrmutils
+                                 libsdmutils libc++ liblog libdrmutils
 
 ifneq ($(TARGET_USES_GRALLOC1), true)
     LOCAL_SHARED_LIBRARIES += libmemalloc